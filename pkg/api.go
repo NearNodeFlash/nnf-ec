0b5e6ac6051c7700ff98d9732d1114535c4da8ab
Nate Roiger <nate.roiger@hpe.com>
2021-05-05 09:01:55 -0500

    Fill out missing endpints for the storage service api

--- pkg/api.go
+++ pkg/api.go
@@ -1,12 +1,15 @@
 package nnf
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
 
-	//nnf "stash.us.cray.com/rabsw/nnf-ec/internal/manager-nnf"
+	nnf "stash.us.cray.com/rabsw/nnf-ec/internal/manager-nnf"
+	server "stash.us.cray.com/rabsw/nnf-ec/internal/manager-server"
 
+	openapi "stash.us.cray.com/rabsw/rfsf-openapi/pkg/common"
 	sf "stash.us.cray.com/rabsw/rfsf-openapi/pkg/models"
 )
 
@@ -38,11 +41,78 @@ func (s *storageService) Capacity() (*sf.CapacityCapacitySource, error) {
 	return model, err
 }
 
-func (s *storageService) ProvisionStorage()
+func (s *storageService) AllocateStorage(capacityBytes int64) (*sf.StoragePoolV150StoragePool, error) {
+	model := new(sf.StoragePoolV150StoragePool)
+
+	model.CapacityBytes = capacityBytes
+	model.Oem = openapi.MarshalOem(nnf.AllocationPolicyOem{
+		Policy:     nnf.SpareAllocationPolicyType,
+		Compliance: nnf.RelaxedAllocationComplianceType,
+	})
+
+	err := s.post("/redfish/v1/StorageServices/NNF/StoragePools", model)
+
+	return model, err
+}
+
+func (s *storageService) CreateStorageGroup(pool *sf.StoragePoolV150StoragePool, endpoint *sf.EndpointV150Endpoint) (*sf.StorageGroupV150StorageGroup, error) {
+	model := new(sf.StorageGroupV150StorageGroup)
+
+	model.Links.StoragePool.OdataId = pool.OdataId
+	model.Links.ServerEndpoint.OdataId = endpoint.OdataId
+
+	err := s.post("/redfish/v1/StorageServices/NNF/StorageGroups", model)
+
+	return model, err
+}
+
+func (s *storageService) CreateFileSystem(pool *sf.StoragePoolV150StoragePool, fileSystem string) (*sf.FileSystemV122FileSystem, error) {
+	model := new(sf.FileSystemV122FileSystem)
+
+	model.Links.StoragePool.OdataId = pool.OdataId
+	model.Oem = openapi.MarshalOem(server.FileSystemOem{
+		Name: fileSystem,
+	})
+
+	err := s.post("/redfish/v1/StorageServices/NNF/FileSystems", model)
+
+	return model, err
+}
+
+func (s *storageService) CreateFileShare(fileSystem *sf.FileSystemV122FileSystem, endpoint *sf.EndpointV150Endpoint, fileSharePath string) (*sf.FileShareV120FileShare, error) {
+	model := new(sf.FileShareV120FileShare)
+
+	model.FileSharePath = fileSharePath
+	model.Links.FileSystem.OdataId = fileSystem.OdataId
+	model.Links.Endpoint.OdataId = endpoint.OdataId
+
+	err := s.post(fileSystem.OdataId+"/ExportedFileShares", model)
+
+	return model, err
+}
 
 func (s *storageService) get(path string, model interface{}) error {
+	return s.do(http.MethodGet, path, model)
+}
 
-	rsp, err := s.client.Get(fmt.Sprintf("http://%s:%s%s", s.address, s.port, path))
+func (s *storageService) post(path string, model interface{}) error {
+	return s.do(http.MethodPost, path, model)
+}
+
+func (s *storageService) do(method string, path string, model interface{}) error {
+	url := fmt.Sprintf("http://%s:%s%s", s.address, s.port, path)
+
+	body := []byte{}
+	if method == http.MethodPost || method == http.MethodPatch {
+		body, _ = json.Marshal(model)
+	}
+
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+
+	rsp, err := s.client.Do(req)
 	if rsp != nil {
 		defer rsp.Body.Close()
 	}
@@ -58,6 +128,6 @@ func (s *storageService) get(path string, model interface{}) error {
 	if err := json.NewDecoder(rsp.Body).Decode(model); err != nil {
 		return err
 	}
-	
+
 	return nil
 }
