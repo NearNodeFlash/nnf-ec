f9e81300eea2d8b734bc398b41308c49e248cdc1
Roiger, Nathaniel <nathaniel.roiger@hpe.com>
2021-08-27 08:21:20 -0500

    Merge pull request #24 in RABSW/nnf-ec from fix/monitor to master
    Squashed commit of the following:
    
    commit 1355cbcb1a8e38da558306813162e7b6d0120bb9
    Author: Nate Roiger <nroiger@gmail.com>
    Date:   Thu Aug 26 15:51:25 2021 -0500
    
        Create an OSX monitor stub for the remote server manager. Slicences build warnings on macs

---
+++ pkg/manager-remote/monitor_osx.go
@@ -0,0 +1,15 @@
+// +build darwin
+
+package remote
+
+type UDevMonitor struct {
+	exit chan struct{}
+}
+
+func (*UDevMonitor) Open() error      { return nil }
+func (*UDevMonitor) Close() error     { return nil }
+func (*UDevMonitor) Run() chan UEvent { return make(chan UEvent, 1) }
+
+type UEvent struct{}
+
+func (*UEvent) IsNvmeEvent() bool { return false }
