fd16cb7186107616bda391d65923c843ea8e7cc5
Nate Roiger <nate.roiger@hpe.com>
2022-07-21 12:40:14 -0500

    Enhance LVM script
    Signed-off-by: Nate Roiger <nate.roiger@hpe.com>

--- tools/lvm.sh
+++ tools/lvm.sh
@@ -23,21 +23,28 @@ Manage LVM volumes on Rabbit
 Usage: $0 COMMAND [ARGS...]
 
 Commands:
-    create [NAME] [NAMESPACE]        create an LVM volume
-    delete [NAME]                    delete an LVM volume
+    list-drives                         list the drives used in create/delete
+    create [NAME] [NAMESPACE-ID]        create an LVM volume all drives
+    delete [NAME]                       delete an LVM volume
 EOF
 }
 
 DRIVES=()
 
 drives() {
-    NAMESPACE=$1
-    for DRIVE in $(ls /dev/nvme* | grep -E "nvme[[:digit:]]+n${NAMESPACE}$");
+    local NAMESPACE=$1
+    for DRIVE in $(ls /dev/nvme* | grep -E "nvme[[:digit:]]+n[[:digit:]]+$");
     do
         if [ "$(nvme id-ctrl ${DRIVE} | grep KIOXIA)" != "" ];
         then
             echo "  Found Kioxia drive ${DRIVE}"
-            DRIVES+="${DRIVE} "
+            NAMESPACEID=$(nvme id-ns ${DRIVE} | grep -E '^NVME Identify Namespace [[:digit:]]+' | awk '{print $4}')
+            if [ "${NAMESPACEID::-1}" == "$NAMESPACE" ];
+            then
+                echo "    Found Namespace ${NAMESPACE}"
+            
+                DRIVES+="${DRIVE} "
+            fi
         fi
     done
 
@@ -47,8 +54,10 @@ drives() {
 NAME=${2:-"rabbit"}
 NAMESPACE=${3:-"1"}
 
-
 case $1 in
+    list-drives)
+        drives $NAMESPACE
+        ;;
     create)
         drives $NAMESPACE
         for DRIVE in ${DRIVES[@]};
