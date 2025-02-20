029bd4b00ba14cad4dc72ee2b1d86aef9a58c038
Anthony Floeder <anthony.floeder@hpe.com>
2025-02-20 12:57:44 -0600

    storage pool/group delete paths must tolerate errors for missing devices and continue storage pool/group database restore path must tolerate missing devices and continue
    Signed-off-by: Anthony Floeder <anthony.floeder@hpe.com>

--- tools/nnf-nvme.sh
+++ tools/nnf-nvme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2022 Hewlett Packard Enterprise Development LP
+# Copyright 2022-2025 Hewlett Packard Enterprise Development LP
 # Other additional copyright holders may be indicated within.
 #
 # The entirety of this work is licensed under the Apache License,
